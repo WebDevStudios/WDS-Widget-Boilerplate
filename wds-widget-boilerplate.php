0270ff23ddc520ff4ff55afc407ebe6a9a76be2c
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-04 17:16:27 -0500

    Fix parse errors caused by using equals (not =>) and semicolons in an array syntax

--- wds-widget-boilerplate.php
+++ wds-widget-boilerplate.php
@@ -96,7 +96,523 @@ class WDS_Widget_Boilerplate extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 
+		echo self::get_widget( array<?php
+
+class MSDN_Blogs_Social_Search_Widget extends WP_Widget {
+
+	/**
+	 * Unique identifier for this widget.
+	 *
+	 * Will also serve as the widget class.
+	 *
+	 * @var string
+	 */
+	protected $widget_slug = 'wds-widget-boilerplate-slug';
+
+	/**
+	 * Widget name displayed in Widgets dashboard.
+	 * Set in __construct since __() shouldn't take a variable.
+	 *
+	 * @var string
+	 */
+	protected $widget_name = '';
+
+	/**
+	 * Shortcode name for this widget
+	 *
+	 * @var string
+	 */
+	protected $shortcode = 'wds_widget';
+
+	/**
+	 * Contruct widget.
+	 */
+	public function __construct() {
+
+		$this->widget_name = _x( 'MSDN Bing Social Search', 'MSDN Bing Social Search widget', 'msdn-social-search' );
+
+		parent::__construct(
+			$this->widget_slug,
+			$this->widget_name,
+			array(
+				'classname'   => $this->widget_slug,
+				'description' => __( 'Replacement for the default search widget wich leverages the bing social search API.', 'msdn-social-search' ),
+			)
+		);
+
+		add_action( 'save_post',    array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
+		add_shortcode( $this->shortcode, array( __CLASS__, 'get_widget' ) );
+	}
+
+	/**
+	 * Delete this widget's cache.
+	 *
+	 * Note: Could also delete any transients
+	 * delete_transient( 'some-transient-generated-by-this-widget' );
+	 */
+	public function flush_widget_cache() {
+		wp_cache_delete( $this->widget_slug, 'widget' );
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @param  array  $args      The widget arguments set up when a sidebar is registered.
+	 * @param  array  $instance  The widget settings as set by user.
+	 */
+	public function widget( $args, $instance ) {
+
+		echo self::<?php
+
+class MSDN_Blogs_Social_Search_Widget extends WP_Widget {
+
+	/**
+	 * Unique identifier for this widget.
+	 *
+	 * Will also serve as the widget class.
+	 *
+	 * @var string
+	 */
+	protected $widget_slug = 'wds-widget-boilerplate-slug';
+
+	/**
+	 * Widget name displayed in Widgets dashboard.
+	 * Set in __construct since __() shouldn't take a variable.
+	 *
+	 * @var string
+	 */
+	protected $widget_name = '';
+
+	/**
+	 * Shortcode name for this widget
+	 *
+	 * @var string
+	 */
+	protected $shortcode = 'wds_widget';
+
+	/**
+	 * Contruct widget.
+	 */
+	public function __construct() {
+
+		$this->widget_name = _x( 'MSDN Bing Social Search', 'MSDN Bing Social Search widget', 'msdn-social-search' );
+
+		parent::__construct(
+			$this->widget_slug,
+			$this->widget_name,
+			array(
+				'classname'   => $this->widget_slug,
+				'description' => __( 'Replacement for the default search widget wich leverages the bing social search API.', 'msdn-social-search' ),
+			)
+		);
+
+		add_action( 'save_post',    array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
+		add_shortcode( $this->shortcode, array( __CLASS__, 'get_widget' ) );
+	}
+
+	/**
+	 * Delete this widget's cache.
+	 *
+	 * Note: Could also delete any transients
+	 * delete_transient( 'some-transient-generated-by-this-widget' );
+	 */
+	public function flush_widget_cache() {
+		wp_cache_delete( $this->widget_slug, 'widget' );
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @param  array  $args      The widget arguments set up when a sidebar is registered.
+	 * @param  array  $instance  The widget settings as set by user.
+	 */
+	public function widget( $args, $instance ) {
+
 		echo self::get_widget( array(
+			'before_widget' => $args['before_widget'],
+			'after_widget'  => $args['after_widget'],
+			'before_title'  => $args['before_title'],
+			'after_title'   => $args['after_title'],
+			'title'         => $instance['title'],
+			'text'          => $instance['text'],
+		) );
+
+	}
+
+	/**
+	 * Return the widget/shortcode output
+	 *
+	 * @param  array  $atts Array of widget/shortcode attributes/args
+	 * @return string       Widget output
+	 */
+	public static function get_widget( $atts ) {
+		$widget = '';
+
+		// Set up default values for attributes
+		$atts = shortcode_atts(
+			array(
+				// Ensure variables<?php
+
+class MSDN_Blogs_Social_Search_Widget extends WP_Widget {
+
+	/**
+	 * Unique identifier for this widget.
+	 *
+	 * Will also serve as the widget class.
+	 *
+	 * @var string
+	 */
+	protected $widget_slug = 'wds-widget-boilerplate-slug';
+
+	/**
+	 * Widget name displayed in Widgets dashboard.
+	 * Set in __construct since __() shouldn't take a variable.
+	 *
+	 * @var string
+	 */
+	protected $widget_name = '';
+
+	/**
+	 * Shortcode name for this widget
+	 *
+	 * @var string
+	 */
+	protected $shortcode = 'wds_widget';
+
+	/**
+	 * Contruct widget.
+	 */
+	public function __construct() {
+
+		$this->widget_name = _x( 'MSDN Bing Social Search', 'MSDN Bing Social Search widget', 'msdn-social-search' );
+
+		parent::__construct(
+			$this->widget_slug,
+			$this->widget_name,
+			array(
+				'classname'   => $this->widget_slug,
+				'description' => __( 'Replacement for the default search widget wich leverages the bing social search API.', 'msdn-social-search' ),
+			)
+		);
+
+		add_action( 'save_post',    array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
+		add_shortcode( $this->shortcode, array( __CLASS__, 'get_widget' ) );
+	}
+
+	/**
+	 * Delete this widget's cache.
+	 *
+	 * Note: Could also delete any transients
+	 * delete_transient( 'some-transient-generated-by-this-widget' );
+	 */
+	public function flush_widget_cache() {
+		wp_cache_delete( $this->widget_slug, 'widget' );
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @param  array  $args      The widget arguments set up when a sidebar is registered.
+	 * @param  array  $instance  The widget settings as set by user.
+	 */
+	public function widget( $args, $instance ) {
+
+		echo self::get_widget( array(
+			'before_widget' = $args['before_widget'],
+			'after_widget'  = $args['after_widget'],
+			'before_title'  = $args['before_title'],
+			'after_title'   = $args['after_title'],
+			'title'         = $instance['title'],
+			'text'          = $instance['text'],
+		) );
+
+	}
+
+	/**
+	 * Return the widget/shortcode output
+	 *
+	 * @param  array  $atts Array of widget/shortcode attributes/args
+	 * @return string       Widget output
+	 */
+	public static function get_widget( $atts ) {
+		$widget = '';
+
+		// Set up default values for attributes
+		$atts = shortcode_atts(
+			array(
+				// Ensure variables
+				'before_widget' => '',
+				'after_widget'  => '',
+				'before_title'  => '',
+				'after_title'   => '',
+				'title'         => '',
+				'text'          => '',
+			),
+			(array) $atts,
+			$this->shortcode
+		);
+
+		// Before widget hook
+		$widget .= $atts['before_widget'];
+
+		// Title
+		$widget .= ( $atts['title'] ) ? $atts['before_title'] . esc_html( $atts['title'] ) . $atts['after_title'] : '';
+
+		$widget .= wpautop( wp_kses_post( $atts['text'] ) );
+
+		// After widget hook
+		$widget .= $atts['after_widget'];
+
+		return $widget;
+	}
+
+	/**
+	 * Update form values as they are saved.
+	 *
+	 * @param  array  $new_instance  New settings for this instance as input by the user.
+	 * @param  array  $old_instance  Old settings for this instance.
+	 * @return array  Settings to save or bool false to cancel saving.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		// Previously saved values
+		$instance = $old_instance;
+
+		// Sanitize title before saving to database
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+
+		// Sanitize text before saving to database
+		if ( current_user_can( 'unfiltered_html' ) ) {
+			$instance['text'] = force_balance_tags( $new_instance['text'] );
+		} else {
+			$instance['text'] = stripslashes( wp_filter_post_kses( addslashes( $new_instance['text'] ) ) );
+		}
+
+		// Flush cache
+		$this->flush_widget_cache();
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form with defaults.
+	 *
+	 * @param  array  $instance  Current settings.
+	 */
+	public function form( $instance ) {
+
+		// If there are no settings, set up defaults
+		$instance = wp_parse_args( (array) $instance,
+			array(
+				'title' => '',
+				'text'  => '',
+			)
+		);
+
+		?>
+
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'msdn-social-search' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_html( $instance['title'] ); ?>" placeholder="optional" /></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Text:', 'msdn-social-search' ); ?></label>
+		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo esc_textarea( $instance['text'] ); ?></textarea></p>
+		<p class="description"><?php _e( 'Basic HTML tags are allowed.', 'msdn-social-search' ); ?></p>
+
+		<?php
+	}
+}
+
+				'before_widget' => '';
+				'after_widget'  => '';
+				'before_title'  => '';
+				'after_title'   => '';
+				'title'         => '';
+				'text'          => '';
+			),
+			(array) $atts,
+			$this->shortcode
+		);
+
+		// Before widget hook
+		$widget .= $atts['before_widget'];
+
+		// Title
+		$widget .= ( $atts['title'] ) ? $atts['before_title'] . esc_html( $atts['title'] ) . $atts['after_title'] : '';
+
+		$widget .= wpautop( wp_kses_post( $atts['text'] ) );
+
+		// After widget hook
+		$widget .= $atts['after_widget'];
+
+		return $widget;
+	}
+
+	/**
+	 * Update form values as they are saved.
+	 *
+	 * @param  array  $new_instance  New settings for this instance as input by the user.
+	 * @param  array  $old_instance  Old settings for this instance.
+	 * @return array  Settings to save or bool false to cancel saving.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		// Previously saved values
+		$instance = $old_instance;
+
+		// Sanitize title before saving to database
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+
+		// Sanitize text before saving to database
+		if ( current_user_can( 'unfiltered_html' ) ) {
+			$instance['text'] = force_balance_tags( $new_instance['text'] );
+		} else {
+			$instance['text'] = stripslashes( wp_filter_post_kses( addslashes( $new_instance['text'] ) ) );
+		}
+
+		// Flush cache
+		$this->flush_widget_cache();
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form with defaults.
+	 *
+	 * @param  array  $instance  Current settings.
+	 */
+	public function form( $instance ) {
+
+		// If there are no settings, set up defaults
+		$instance = wp_parse_args( (array) $instance,
+			array(
+				'title' => '',
+				'text'  => '',
+			)
+		);
+
+		?>
+
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'msdn-social-search' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_html( $instance['title'] ); ?>" placeholder="optional" /></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Text:', 'msdn-social-search' ); ?></label>
+		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo esc_textarea( $instance['text'] ); ?></textarea></p>
+		<p class="description"><?php _e( 'Basic HTML tags are allowed.', 'msdn-social-search' ); ?></p>
+
+		<?php
+	}
+}
+( array(
+			'before_widget' = $args['before_widget'],
+			'after_widget'  = $args['after_widget'],
+			'before_title'  = $args['before_title'],
+			'after_title'   = $args['after_title'],
+			'title'         = $instance['title'],
+			'text'          = $instance['text'],
+		) );
+
+	}
+
+	/**
+	 * Return the widget/shortcode output
+	 *
+	 * @param  array  $atts Array of widget/shortcode attributes/args
+	 * @return string       Widget output
+	 */
+	public static function get_widget( $atts ) {
+		$widget = '';
+
+		// Set up default values for attributes
+		$atts = shortcode_atts(
+			array(
+				// Ensure variables
+				'before_widget' => '';
+				'after_widget'  => '';
+				'before_title'  => '';
+				'after_title'   => '';
+				'title'         => '';
+				'text'          => '';
+			),
+			(array) $atts,
+			$this->shortcode
+		);
+
+		// Before widget hook
+		$widget .= $atts['before_widget'];
+
+		// Title
+		$widget .= ( $atts['title'] ) ? $atts['before_title'] . esc_html( $atts['title'] ) . $atts['after_title'] : '';
+
+		$widget .= wpautop( wp_kses_post( $atts['text'] ) );
+
+		// After widget hook
+		$widget .= $atts['after_widget'];
+
+		return $widget;
+	}
+
+	/**
+	 * Update form values as they are saved.
+	 *
+	 * @param  array  $new_instance  New settings for this instance as input by the user.
+	 * @param  array  $old_instance  Old settings for this instance.
+	 * @return array  Settings to save or bool false to cancel saving.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		// Previously saved values
+		$instance = $old_instance;
+
+		// Sanitize title before saving to database
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+
+		// Sanitize text before saving to database
+		if ( current_user_can( 'unfiltered_html' ) ) {
+			$instance['text'] = force_balance_tags( $new_instance['text'] );
+		} else {
+			$instance['text'] = stripslashes( wp_filter_post_kses( addslashes( $new_instance['text'] ) ) );
+		}
+
+		// Flush cache
+		$this->flush_widget_cache();
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form with defaults.
+	 *
+	 * @param  array  $instance  Current settings.
+	 */
+	public function form( $instance ) {
+
+		// If there are no settings, set up defaults
+		$instance = wp_parse_args( (array) $instance,
+			array(
+				'title' => '',
+				'text'  => '',
+			)
+		);
+
+		?>
+
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'msdn-social-search' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_html( $instance['title'] ); ?>" placeholder="optional" /></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Text:', 'msdn-social-search' ); ?></label>
+		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo esc_textarea( $instance['text'] ); ?></textarea></p>
+		<p class="description"><?php _e( 'Basic HTML tags are allowed.', 'msdn-social-search' ); ?></p>
+
+		<?php
+	}
+}
+(
 			'before_widget' = $args['before_widget'],
 			'after_widget'  = $args['after_widget'],
 			'before_title'  = $args['before_title'],
